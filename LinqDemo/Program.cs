88ae2d1e0b11092a8fc1124d2ce43770431d2888
gwarner3 <george@georgedevelops.com>
2017-08-29 16:21:01 -0500

    Added Employee and Worked classes, instatniated some employees

--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace LinqDemo
 {
+    class Employee
+    {
+        public int EmployeeID { get; set; }
+    }
+    class Worked
+    {
+        public int WorkerID { get; set; }
+        public int HoursWorked { get; set; }        
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Employee frank = new Employee { EmployeeID = 10 };
+            Employee joe = new Employee { EmployeeID = 20 };
+            Employee will = new Employee { EmployeeID = 30 };
+            Employee renee = new Employee { EmployeeID = 40 };
         }
     }
 }
